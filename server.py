938a1379fd6a0a0011c0d6d5b1519f4646baab0f
ap-cf-test <74004973+ap-cf-test@users.noreply.github.com>
2020-11-05 16:15:26 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("XXXXX")
+httpd.serve_forever()
